7bebdbefcf56793c40afa744aa08efd6c3092d1a
Timothy Meyer <dgnt22@gmail.com>
2022-07-15 15:50:44 -0700

    h

--- Die.gd
+++ Die.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var faces = [$SimpleDie.attack, $SimpleDie.attack, $SimpleDie.attack, 
-$SimpleDie.defend, $SimpleDie.defend, $SimpleDie.defend]
+var faces = [Global.attack, Global.attack, Global.attack, 
+Global.defend, Global.defend, Global.defend]
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,13 +10,13 @@ $SimpleDie.defend, $SimpleDie.defend, $SimpleDie.defend]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Unfold/Background/F1.set_face(faces[0])
-	$Unfold/Background/F2.set_face(faces[1])
-	$Unfold/Background/F3.set_face(faces[2])
-	$Unfold/Background/F4.set_face(faces[3])
-	$Unfold/Background/F5.set_face(faces[4])
-	$Unfold/Background/F6.set_face(faces[5])
-	$Unfold.popup()
+	$Background/F1.set_face(faces[0])
+	$Background/F2.set_face(faces[1])
+	$Background/F3.set_face(faces[2])
+	$Background/F4.set_face(faces[3])
+	$Background/F5.set_face(faces[4])
+	$Background/F6.set_face(faces[5])
+	$Background.hide()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,6 +25,11 @@ func _ready():
 
 
 func _on_Area2D_mouse_entered():
-	$Unfold.popup()
+	$Background.show()
 	print("HI")
+	pass # Replace with function body.
+
+
+func _on_Area2D_mouse_exited():
+	$Background.hide()
 	pass # Replace with function body.
--- Global.gd
+++ Global.gd
@@ -1,16 +1,17 @@
-extends Node2D
+extends Node
 
-#var faces = [attack, attack, defend, defend, defend, defend]
+var attack = preload("res://art/Weapon.png")
+var defend = preload("res://art/shield.png")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func set_face(value):
-	$Sprite.texture = value
-	$Sprite.scale[0] = 40.0/value.get_width()
-	$Sprite.scale[1] = 40.0/value.get_height()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
