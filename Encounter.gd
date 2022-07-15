fc39231ba0df918e7a8b6ce30aa388b9a5290cad
Alexander Wilson <awlego@gmail.com>
2022-07-15 14:46:29 -0700

    adding victory and defeat text as well as health clamping

--- Encounter.gd
+++ Encounter.gd
@@ -5,6 +5,7 @@ extends Node
 # var a = 2
 # var b = "text"
 var next_dice_slot = 0
+const MAX_HEALTH = 10000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,7 +16,12 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# I could just update all text here... it's less efficient but it means I don't
+# have to do the update everywhere else -- all the updates are here in one function
 #func _process(delta):
+#	$PlayerHealth.text = str($Player.health)
+#	$PlayerEnergy.text = str($Player.energy)
+#	$EnemyHealth.text = str($Enemy.health)
 #	pass
 
 func _on_RollButton_pressed():
@@ -42,11 +48,15 @@ func roll_dice():
 	var dice_slots = get_tree().get_nodes_in_group("DiceSlots")
 	var dice_to_roll = dice_slots[next_dice_slot]
 	dice_to_roll.frame = dice_roll
-	$EnemyHealth.text = str(int($EnemyHealth.text) - (dice_roll + 1))
+	$Enemy.health -= (dice_roll + 1)
+	$Enemy.health = clamp($Enemy.health, 0, MAX_HEALTH)
+	$EnemyHealth.text = str($Enemy.health)
 	next_dice_slot += 1
 	
 	# do effect of rolled die
 	# check for victory (where exactly should this live?)
+	if $Enemy.health == 0:
+		round_cleared()
 	
 	# if energy = 0, end turn and start enemy turn
 	if $Player.energy == 0:
@@ -56,17 +66,24 @@ func roll_dice():
 #
 func enemy_turn():
 	$Player.health -= 3
+	$Player.health = clamp($Player.health, 0, MAX_HEALTH)
 	$PlayerHealth.text = str($Player.health)
 #	enemy.do_current_action()
 #	enemy.select_next_action()
+
+	if $Player.health == 0:
+		game_over()
+
 	# go start player turn, which means resetting their energy/cds
 	start_player_turn()
 	
 func game_over():
+	$MessageBox.text = "Game over!"
 	# display end of game info and return to home screen
 	pass
 	
 func round_cleared():
+	$MessageBox.text = "You beat the encounter!"
 	# display round victory info and return to stage selection
 	pass
 
